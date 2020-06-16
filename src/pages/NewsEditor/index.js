0a6fd1f2560670a70d4dc2faf512c73ce2de04e8
Alexuhugugul <thecrosssama@gmail.com>
2020-06-17 03:39:23 +0700

    merge LD-7_new

--- src/pages/NewsEditor/index.js
+++ src/pages/NewsEditor/index.js
@@ -20,7 +20,7 @@ export default test;   */
 
 import React, { Component } from 'react';
 /* import { EditorState, convertToRaw } from 'draft-js'; */
-import { Editor } from 'react-draft-wysiwyg';
+/* import { Editor } from 'react-draft-wysiwyg'; */
 /* import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs'; */
 import { Button } from 'semantic-ui-react';
@@ -47,12 +47,12 @@ class EditorConvertToHTML extends Component {
         <h2>{getTranslation("Preview")}</h2>
         <textarea className="preview"></textarea>
         <h2>{getTranslation("Text news")}</h2>
-        <Editor
+      {/*   <Editor
           editorState={editorState}
           onEditorStateChange={this.onEditorStateChange}
           wrapperClassName="my-wrapper"
           editorClassName="my-editor-class"
-        />
+        /> */}
         <div className="date">
           <div className="input-date" >
             <h3>{getTranslation("Publication date")}</h3>

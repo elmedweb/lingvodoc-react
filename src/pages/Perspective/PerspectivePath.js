cb7f595e58b5e203955caa8ea1002f7e901f14e4
Alexuhugugul <thecrosssama@gmail.com>
2020-09-16 00:17:48 +0700

    fix error

--- src/pages/Perspective/PerspectivePath.js
+++ src/pages/Perspective/PerspectivePath.js
@@ -45,69 +45,72 @@ const queryAvailablePerspectives = gql`
  * Perspective breadcrumb component.
  */
 class PerspectivePath extends React.Component {
-
   constructor(props) {
     super(props);
   }
 
-  getCommonTreeSection ({ treeElement }) {
+  getCommonTreeSection({ treeElement }) {
     const section = {
-      key:     null,
+      key: null,
       content: null
     };
 
-    section.key     = treeElement.id;
-    section.link    = true;
+    section.key = treeElement.id;
+    section.link = true;
     section.content = treeElement.translation;
-    section.as      = Link;
-    section.to = `/dashboard/dictionaries_all?anchor=${ treeElement.id }`;
+    section.as = Link;
+    section.to = `/?anchor=${treeElement.id}`;
 
     return section;
-  };
+  }
 
-  getPerspectiveTreeSection ({ treeElement, perspectives, mode, dictionary_id, tree, id, actions }) {
+  getPerspectiveTreeSection({
+    treeElement, perspectives, mode, dictionary_id, tree, id, actions
+  }) {
     const section = {
-      key:     null,
+      key: null,
       content: null
     };
 
-    section.key     = treeElement.id;
-    section.active  = true;
-    section.content = <Dropdown inline text={ treeElement.translation }>
-                        <Dropdown.Menu>
-                            {perspectives.filter(perspective => perspective.id != tree[0].id).map(perspective => (
-                            <Dropdown.Item
-                              as = 'a'
-                              key = { perspective.id }
-                              href = { `${ window.location.protocol }//${ window.location.host }/dictionary/${ perspective.parent_id.join( '/' ) }/perspective/${ perspective.id.join( '/' ) }/${ mode }` }
-                              icon = 'chevron right'
-                              text = { perspective.translation }
-                            />))
+    section.key = treeElement.id;
+    section.active = true;
+    section.content = <Dropdown inline text={treeElement.translation}>
+      <Dropdown.Menu>
+        {perspectives.filter(perspective => perspective.id != tree[0].id).map(perspective => (
+          <Dropdown.Item
+            as="a"
+            key={perspective.id}
+            href={`${window.location.protocol}//${window.location.host}/dictionary/${perspective.parent_id.join('/')}/perspective/${perspective.id.join('/')}/${mode}`}
+            icon="chevron right"
+            text={perspective.translation}
+          />))
                             }
-                          <Dropdown.Divider />
-                          <Dropdown.Item
-                            icon = 'users'
-                            text = { getTranslation( `'${ treeElement.translation }' roles...` ) }
-                            onClick = { () => actions.openRoles( id, 'perspective' ) }
-                          />
-                          <Dropdown.Item
-                            icon = 'setting'
-                            text = { getTranslation( `'${ treeElement.translation }' properties...` ) }
-                            onClick = { () => actions.openPerspectivePropertiesModal( id, dictionary_id ) }
-                          />
-                          <Dropdown.Item
-                            icon = 'percent'
-                            text = { getTranslation( `'${ treeElement.translation }' statistics...` ) }
-                            onClick = { () => actions.openStatistics( id, 'perspective' ) }
-                          />
-                        </Dropdown.Menu>
+        <Dropdown.Divider />
+        <Dropdown.Item
+          icon="users"
+          text={getTranslation(`'${treeElement.translation}' roles...`)}
+          onClick={() => actions.openRoles(id, 'perspective')}
+        />
+        <Dropdown.Item
+          icon="setting"
+          text={getTranslation(`'${treeElement.translation}' properties...`)}
+          onClick={() => actions.openPerspectivePropertiesModal(id, dictionary_id)}
+        />
+        <Dropdown.Item
+          icon="percent"
+          text={getTranslation(`'${treeElement.translation}' statistics...`)}
+          onClick={() => actions.openStatistics(id, 'perspective')}
+        />
+      </Dropdown.Menu>
                       </Dropdown>;
 
     return section;
   }
 
   render() {
-    const { id, dictionary_id, queryPerspectivePath, queryAvailablePerspectives, mode, className, actions, user } = this.props;
+    const {
+      id, dictionary_id, queryPerspectivePath, queryAvailablePerspectives, mode, className, actions, user
+    } = this.props;
 
     if (queryPerspectivePath.loading || queryPerspectivePath.error || queryAvailablePerspectives.loading || queryAvailablePerspectives.error) {
       return null;
@@ -117,18 +120,19 @@ class PerspectivePath extends React.Component {
     const { perspectives } = queryAvailablePerspectives.dictionary;
 
     const sections = tree.slice().reverse().map((e, index) => {
-      if ( perspectives.length > 1 && index == tree.length - 1 ) {
-        return this.getPerspectiveTreeSection({ treeElement: e, perspectives, mode, dictionary_id, tree, id, actions })
-      } else {
-        return this.getCommonTreeSection({ treeElement: e });
+      if (perspectives.length > 1 && index == tree.length - 1) {
+        return this.getPerspectiveTreeSection({
+          treeElement: e, perspectives, mode, dictionary_id, tree, id, actions
+        });
       }
+      return this.getCommonTreeSection({ treeElement: e });
     });
- 
+
     return (
       <Header as="h2" className={className}>
         <Breadcrumb
           icon="right angle"
-          sections={ sections }
+          sections={sections}
         />
       </Header>
     );
@@ -148,13 +152,16 @@ export default compose(
     state => state.user,
     dispatch => ({
       actions: bindActionCreators(
-        {openDictionaryOrganizationsModal,
+        {
+          openDictionaryOrganizationsModal,
           openDictionaryPropertiesModal,
           openPerspectivePropertiesModal,
           openRoles,
           openSaveDictionaryModal,
-          openStatistics},
-        dispatch),
+          openStatistics
+        },
+        dispatch
+      ),
     })
   ),
   graphql(queryPerspectivePath, {
